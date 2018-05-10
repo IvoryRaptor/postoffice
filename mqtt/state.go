abbeaa110dbf35d3208f407dbbf16eacf86f251b
fjl716 <fjl716@163.com>
2018-05-10 16:58:35 +0800

    修改结构

--- mqtt/state.go
+++ mqtt/state.go
@@ -4,7 +4,6 @@ import "github.com/IvoryRaptor/postoffice"
 
 func (m *MQTT)Config(kernel postoffice.IKernel, config map[string]interface{}) error {
 	m.kernel = kernel
-	print("*********", kernel)
 	m.config = config
 	return nil
 }
